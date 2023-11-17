4240a5cdccde093d90a8cd13a8479989a48443de
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-11-17 15:43:46 +0900

    상품 등록 처리 컨트롤러 구현
    @RequestParam. @ModelAttribute 를 활용한 4가지 상품 등록 컨트롤러 구현

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -69,6 +67,70 @@ public class BasicItemController {
     public String addForm(){
         return "basic/addForm";
     }
+
+    /**
+     * 상품 등록 처리1 (@RequestParam 사용)
+     * @return
+     */
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model){
+
+        Item item = new Item();
+
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     * 상품 등록 처리2
+     * @ModelAttribute 사용, model.addAttribute("item", item)
+     * @param item
+     * @param model
+     * @return
+     */
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+
+    /**
+     * 상품 등록 처리3
+     * 파라미터 Model model 생략, Item 객체는 item 이름 (클래스 첫글자를 소문자로 바꾼 이름)으로 자동 추가
+     * @param item
+     * @return
+     */
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * 상품 등록 처리4, @ModelAttribute 자체 생략
+     * @param item
+     * @return
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
 
     /**
      *  테스트용 데이터 추가
